8105e65bc5eafba5cd4df34c2b8641bee095c12b
magdalenagomezf <2302577@ucc.edu.ar>
2024-06-17 21:29:32 -0300

    cambios en amigo invisble

--- Sortea2.js
+++ Sortea2.js
@@ -1,19 +1,19 @@
+
 /**
  * Realiza un sorteo entre los participantes ingresados y dibuja los ganadores en un canvas
  * @function realizarSorteo
- * @return {void}
  */
 const realizarSorteo = () => {
-    const titulo = document.getElementById('IngresoDatos').value; //obtiene el valor ingresado en la en el campo "IngresoDatos" y lo asigna titulo
-    const numeroPremios = parseInt(document.getElementById('Numeropremios').value); //obtiene el valor ingresado en la en el campo "NumeroPremios", lo convierte a entero con parseInt() y lo asigna numeroPremios
-    const listaParticipantes = document.getElementById('ListaParticipantes').value.trim().split('\n');//obtiene el valor ingresado en la en el campo "ListaParticipantes", borra los espacios en blanco del principio y del final con trim(), divide el texto en lineas con split("\n") y guarda cada linea como un elemento del arreglo listaParticipantes
+    const titulo = document.getElementById('IngresoDatos').value;
+    const numeroPremios = parseInt(document.getElementById('Numeropremios').value);
+    const listaParticipantes = document.getElementById('ListaParticipantes').value.trim().split('\n');
 
-    const ganadores = []; //arreglo que guardará el nombre de los ganadores
-    const participantes = [...listaParticipantes]; //copia del arreglo listaParticipantes, para que no cambie el arreglo original cuando se hacen sorteos
+    const ganadores = [];
+    const participantes = [...listaParticipantes];
 
-    while (ganadores.length < numeroPremios) { //se hará hasta que se hayan seleccionado la cantidad de numeroPremios ganadores
-        const indice = Math.floor(Math.random() * participantes.length); //genera un numero aleatorio entre 0 y participantes[] y lo guarda en indice
-        ganadores.push(participantes.splice(indice, 1)[0]); //saca un elemento de participantes[] en el indice y lo elimina de participantes[], despues lo agrega a ganadores[]
+    while (ganadores.length < numeroPremios) {
+        const indice = Math.floor(Math.random() * participantes.length);
+        ganadores.push(participantes.splice(indice, 1)[0]);
     }
     dibujarEnCanvas(titulo, ganadores);
 };
@@ -21,7 +21,6 @@ const realizarSorteo = () => {
 /**
  * Borra el contenido del campo Lista de Participantes
  * @function borrarParticipantes
- * @returns {void}
  */
 const borrarParticipantes = () => {
     document.getElementById('ListaParticipantes').value = '';
@@ -30,11 +29,10 @@ const borrarParticipantes = () => {
 /**
  * Comprueba que los valores ingresados sean correctos
  * @function comprobarValores
- * @returns {void}
  */
 const comprobarValores = () => {
-    const numeroPremios = parseInt(document.getElementById('Numeropremios').value); //obtiene el valor ingresado en la en el campo "NumeroPremios", lo convierte a entero con parseInt() y lo asigna numeroPremios
-    const listaParticipantes = document.getElementById('ListaParticipantes').value.trim().split('\n');//obtiene el valor ingresado en la en el campo "ListaParticipantes", borra los espacios en blanco del principio y del final con trim(), divide el texto en lineas con split("\n") y guarda cada linea como un elemento del arreglo listaParticipantes
+    const numeroPremios = parseInt(document.getElementById('Numeropremios').value);
+    const listaParticipantes = document.getElementById('ListaParticipantes').value.trim().split('\n');
 
     if (listaParticipantes.length < 2) {
         alert('Error, mínimo de 2 participantes');
@@ -52,9 +50,8 @@ const comprobarValores = () => {
 /**
  * Dibuja en el canvas
  * @function dibujarEnCanvas
- * @param {string} titulo - El título del sorteo
- * @param {array} ganadores - Lista de ganadores
- * @returns {void}
+ * @param titulo {string} - El título del sorteo
+ * @param ganadores {array} - Lista de ganadores
  */
 const dibujarEnCanvas = (titulo, ganadores) => {
     const canvas = document.getElementById('myCanvas');
@@ -63,11 +60,11 @@ const dibujarEnCanvas = (titulo, ganadores) => {
 
     ctx.font = '16px Calibri';
     ctx.fillStyle = 'black';
-    ctx.fillText('Título: ${titulo}', 10, 20);
+    ctx.fillText(`Título: ${titulo}`, 10, 20);
     ctx.fillText('Ganadores:', 10, 40);
 
-    ganadores.forEach((ganador, index) => { //recorre el arreglo ganadores, donde ganador es cada elemento del arreglo y indice es el indice actual del elemento
-        ctx.fillText('${index + 1}. ${ganador}', 10, 60 + (index * 20)); //dibuja cada ganador en el canvas
+    ganadores.forEach((ganador, index) => {
+        ctx.fillText(`${index + 1}. ${ganador}`, 10, 60 + (index * 20));
     });
 };
 
@@ -78,7 +75,7 @@ const realizarAmigoInvisible = () => {
         alert('Error, mínimo de 2 participantes');
         return;
     }
-    listaParticipantes.toLowerCase(); //Conversión a minúsculas
+    listaParticipantes.toLowerCase();
     const parti_que_regala = [listaParticipantes];
     const destinario = [];
 
@@ -111,7 +108,5 @@ const dibujarEnCanvas2 = (participante, destinatario) => {
     ctx.font = '16px Calibri';
     ctx.fillStyle = 'black';
     ctx.fillText(' ${participante}', 10, 20);
-    ctx.fillText(`&{destinatario}`, 10, 40);
-
-
+    ctx.fillText('&{destinatario}', 10, 40);
 }
